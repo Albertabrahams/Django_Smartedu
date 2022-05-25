da541b9d67e3224d09da4c4324e3f4f0de685c7d
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-05-25 11:06:12 +0300

    tags

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Category, Tag
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'available')
     list_filter = ('available',)
     search_fields = ('name', "description")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -1,9 +1,22 @@
-from distutils.command import upload
-from email.policy import default
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
 class Course(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name= "Kurs Adı", help_text= "Kurs adını yazınız")
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to= "media/%Y %m %d/", default="media/courseisnot.png")
     date = models.DateTimeField(auto_now=True)
