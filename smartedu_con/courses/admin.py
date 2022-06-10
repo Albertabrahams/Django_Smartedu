0cef8612875b16bb815cc1c9f92e221dc29436dc
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-10 23:34:55 +0300

    1.4

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -1,12 +1,38 @@
 from django.contrib import admin
-
+from django.utils import timezone
 from .models import Course, Category, Tag
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'available')
-    list_filter = ('available',)
+    list_display = ('name', 'available', 'date', 'description', 'updated_since')
+    list_filter = ('available', "date",)
+    ordering = ("name",)
     search_fields = ('name', "description")
+    list_editable = ("available",)
+    list_per_page = 3
+    date_hierarchy = "date"
+    actions = ("available",)
+    fieldsets = (
+        (None, {
+            "fields": (
+                ("name", "teacher"), "available"
+            ),
+        }),
+        ("Advanced options", {
+            "fields": ("description",),
+            "classes": ("collapse",),
+            "description": "you can write here"
+        })
+    )
+    def available(self, request, queryset):
+        count = queryset.update(available=True)
+        self.message_user(request, f"{count} lesson changed")
+    
+    available.short_description = 'Make available'
+
+    def updated_since(self, course):
+        difference = timezone.now() - course.date
+        return difference.days 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
