980466bd6ea1b18575569273fc923bdb9da13fd7
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-22 14:18:26 +0300

    project done

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%i)*_b5glqazqf6bcld^m@*zm=phil19(84cyztey50_evw+9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-smartedu-project.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
